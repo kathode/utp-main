8fb3b8702b0a6848f4b2619e17e1de54ef7a33b0
kathode <pfribeiro@gmail.com>
2018-03-31 13:41:11 +0100

    Update cloud_build.sh

--- bin/cloud_build.sh
+++ bin/cloud_build.sh
@@ -15,7 +15,7 @@ bash "$BIN_DIR/utp_deps.sh"
 ROOT=$ISABELLE_UTP
 
 # Build all heap images of Isabelle/UTP
-isabelle="docker run --mount source=$TRAVIS_BUILD_DIR,target=$CONTAINER_ISABELLE_UTP makarius/isabelle:Isabelle2017"
+isabelle="docker run --mount type=bind,source=$TRAVIS_BUILD_DIR,target=$CONTAINER_ISABELLE_UTP makarius/isabelle:Isabelle2017"
 printf "\nBuilding Isabelle/UTP sessions... \n\n"
 
 dirs=( "toolkit" "utp" "theories/designs" "theories/reactive" "theories/rea_designs" "theories/circus" "tutorial" )
@@ -23,10 +23,12 @@ heaps=( "UTP-Toolkit" "UTP" "UTP-Designs" "UTP-Reactive" "UTP-Reactive-Designs" 
 
 for ((i=0;i<${#heaps[@]};++i));
 do
+	# Need to give write permissions to docker container to write to same directory. No obvious alternative.
+	chmod +o+w "$ISABELLE_UTP/${dirs[i]}"
 	$isabelle build -d $CONTAINER_ISABELLE_UTP -d $CONTAINER_ISABELLE_UTP/contrib -b "${heaps[i]}" || exit
         if [ -f "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" ]; then
                 echo "Installing ${heaps[i]} documentation to doc/${heaps[i]}.pdf..."
 		cp "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" "$ISABELLE_UTP/doc/${heaps[i]}.pdf"
         fi
-
+	chmod +o-w "$ISABELLE_UTP/${dirs[i]}"
 done
